e5bb6b44f06b74ff0f95e43196dc01edacb459f1
Mike Filicetti <filicetti.michael@gmail.com>
2018-12-17 14:06:24 -0800

    fixed file path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const pg = require('pg');
 // DATABASE CONFIG
 const dbaddress = process.env.DATABASE_URL;
 const client = new pg.Client(dbaddress);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express(); // TODO: add pg URL
 
 // CONNECT TO DB
@@ -21,6 +21,13 @@ app.use(express.static('./public'));
 
 // SETUP VIEW ENGINE
 app.set('view engine', 'ejs');
+
+// ROUTES
+app.get('/', getIndex);
+
+function getIndex(req, res) {
+  res.render('index');
+}
 
 // ERROR MANAGEMENT
 app.get('*', (req, res) => res.status(404).send('Not Found'));
