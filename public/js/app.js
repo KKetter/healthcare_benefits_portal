a3273e43164f8a77a1f777ea7dbe143954bc5027
Mike Filicetti <filicetti.michael@gmail.com>
2018-12-19 19:18:57 -0800

    initial styling draft complete and hamburger functioning

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@
 //Get Started form becomes visible to user
 $ ('#finderButton').on('click', function(event){
   event.preventDefault();
-  console.log('clicked');
+  console.log('finder button clicked');
   $('#form').removeClass('hide-form');
   $('#form').addClass('show-form');
 });
@@ -14,4 +14,3 @@ $ ('#submitButton').on('click', function(event){
   event.preventDefault();
   location.assign('/doctors');
 });
-
